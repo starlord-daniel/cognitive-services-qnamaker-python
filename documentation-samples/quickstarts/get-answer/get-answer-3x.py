3004ae9e1e3818973059302b46b36414588498bb
Daniel Heinze <daheinze@microsoft.com>
2019-10-24 09:31:24 +0200

    Updated get-answer-3x.py
    Updated the code to be more pythonic and convenient to the developer using the resources

--- documentation-samples/quickstarts/get-answer/get-answer-3x.py
+++ documentation-samples/quickstarts/get-answer/get-answer-3x.py
@@ -1,45 +1,56 @@
-import http.client, urllib.parse, json, time, sys
+import requests
+import json
+
+# Represents the various elements used to create HTTP request URIs
+# for QnA Maker operations.
+# From Publish Page of the Qna Maker website
+# Exact value of the host field
+host = 'https://YOUR-RESOURCE-NAME.azurewebsites.net/qnamaker'
+
+# Management APIs postpend the version to the route
+# From Publish Page
+# Example: /knowledgebases/ZZZ15f8c-d01b-4698-a2de-85b0dbf3358c/generateAnswer
+# Is the exact value after POST
+endpoint = '/knowledgebases/YOUR-KBID/generateAnswer'
+
+# Authorization endpoint key
+# From Publish Page
+endpoint_key = "YOUR-ENDPOINT-KEY"
+
+qna_url = host + endpoint
+
+# The headers of the request to qna maker
+# They don't need to be changed
+headers = {
+    "Authorization": f"EndpointKey {endpoint_key}",
+    "Content-Type": "application/json"
+}
 
 
+def get_qna_response(question, qna_url, headers):
+    '''
+    This function returns the qna maker response to your question.
+    '''
+    data = {
+        "question": question
+    }
 
-  # Represents the various elements used to create HTTP request URIs
-  # for QnA Maker operations.
-  # From Publish Page
-  # Example: YOUR-RESOURCE-NAME.azurewebsites.net
-  # CAUTION: This is not the exact value of HOST field
-  # HOST trimmed to work with http library
-  host = "YOUR-RESOURCE-NAME.azurewebsites.net";
+    try:
+        # make a post call to the qna maker service
+        response = requests.post(url=qna_url,
+                                 data=json.dumps(data),
+                                 headers=headers)
+        return response.json()
+    except Exception as e:
+        # An exception occured - display it to the console
+        print(e)
 
-  # Authorization endpoint key
-  # From Publish Page
-  endpoint_key = "YOUR-ENDPOINT-KEY";
 
-  # Management APIs postpend the version to the route
-  # From Publish Page
-  # Example: /knowledgebases/ZZZ15f8c-d01b-4698-a2de-85b0dbf3358c/generateAnswer
-  # CAUTION: This is not the exact value after POST
-  # Part of HOST is prepended to route to work with http library
-  route = "/qnamaker/knowledgebases/e7015f8c-d01b-4698-a2de-85b0dbf3358c/generateAnswer";
+# Put your question here
+question = "Is the QnA Maker Service free?"
 
-  # JSON format for passing question to service
-  question = "{'question': 'Is the QnA Maker Service free?','top': 3}";
+# Get the answer from the function defined above
+answer = get_qna_response(question, qna_url, headers)
 
-  headers = {
-    'Authorization': 'EndpointKey ' + endpoint_key,
-    'Content-Type': 'application/json'
-  }
-
-try:
-  conn = http.client.HTTPSConnection(host,port=443)
-
-  conn.request ("POST", route,  question, headers)
-
-  response = conn.getresponse ()
-
-  answer = response.read ()
-
-  print(json.dumps(json.loads(answer), indent=4))
-
-except :
-    print ("Unexpected error:", sys.exc_info()[0])
-    print ("Unexpected error:", sys.exc_info()[1])
+# print the answer with indentation (which is more readable
+print(json.dumps(answer, indent=4))
